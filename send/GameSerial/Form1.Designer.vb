0e06a827f3723f3b0b27cefd493df8804b607c0d
tokudairobocon <tokudairobocon@gmail.com>
2021-05-11 21:23:20 +0900

    送信側

---
+++ send/GameSerial/Form1.Designer.vb
@@ -0,0 +1,129 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class EV3
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.SerialPort = New System.IO.Ports.SerialPort(Me.components)
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Controller = New System.Windows.Forms.PictureBox()
+        Me.ComNum = New System.Windows.Forms.ComboBox()
+        Me.Connect = New System.Windows.Forms.Button()
+        Me.ConsoleBox = New System.Windows.Forms.TextBox()
+        Me.State = New System.Windows.Forms.TextBox()
+        CType(Me.Controller, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'SerialPort
+        '
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 100000
+        '
+        'Controller
+        '
+        Me.Controller.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.Controller.ErrorImage = Global.GameSerial.My.Resources.Resources.a960x540l
+        Me.Controller.Image = Global.GameSerial.My.Resources.Resources.Red
+        Me.Controller.InitialImage = Global.GameSerial.My.Resources.Resources.Red
+        Me.Controller.Location = New System.Drawing.Point(0, 0)
+        Me.Controller.Name = "Controller"
+        Me.Controller.Size = New System.Drawing.Size(284, 219)
+        Me.Controller.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.Controller.TabIndex = 20
+        Me.Controller.TabStop = False
+        Me.Controller.WaitOnLoad = True
+        '
+        'ComNum
+        '
+        Me.ComNum.FormattingEnabled = True
+        Me.ComNum.Location = New System.Drawing.Point(0, 0)
+        Me.ComNum.Name = "ComNum"
+        Me.ComNum.Size = New System.Drawing.Size(90, 20)
+        Me.ComNum.TabIndex = 22
+        '
+        'Connect
+        '
+        Me.Connect.Location = New System.Drawing.Point(107, -2)
+        Me.Connect.Name = "Connect"
+        Me.Connect.Size = New System.Drawing.Size(75, 23)
+        Me.Connect.TabIndex = 0
+        Me.Connect.Text = "接続！"
+        Me.Connect.UseVisualStyleBackColor = True
+        '
+        'ConsoleBox
+        '
+        Me.ConsoleBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.ConsoleBox.Location = New System.Drawing.Point(0, 225)
+        Me.ConsoleBox.MaxLength = 100
+        Me.ConsoleBox.Multiline = True
+        Me.ConsoleBox.Name = "ConsoleBox"
+        Me.ConsoleBox.ReadOnly = True
+        Me.ConsoleBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.ConsoleBox.Size = New System.Drawing.Size(284, 150)
+        Me.ConsoleBox.TabIndex = 25
+        '
+        'State
+        '
+        Me.State.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.State.Location = New System.Drawing.Point(0, 381)
+        Me.State.MaxLength = 100
+        Me.State.Multiline = True
+        Me.State.Name = "State"
+        Me.State.ReadOnly = True
+        Me.State.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.State.Size = New System.Drawing.Size(284, 79)
+        Me.State.TabIndex = 26
+        '
+        'EV3
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(284, 461)
+        Me.Controls.Add(Me.State)
+        Me.Controls.Add(Me.ConsoleBox)
+        Me.Controls.Add(Me.Connect)
+        Me.Controls.Add(Me.ComNum)
+        Me.Controls.Add(Me.Controller)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "EV3"
+        Me.ShowIcon = False
+        Me.Text = "EV3"
+        CType(Me.Controller, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents SerialPort As IO.Ports.SerialPort
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Controller As PictureBox
+    Friend WithEvents ComNum As ComboBox
+    Friend WithEvents Connect As Button
+    Friend WithEvents ConsoleBox As TextBox
+    Friend WithEvents State As TextBox
+End Class
---
+++ send/GameSerial/Form1.vb
@@ -0,0 +1,199 @@
+﻿'                                              ```                                                   
+'                                              sy/                                                   
+'                    L2 6                      hhy-                  R2 7                                
+'                    L1 4                     ohhh/                  R1 5
+'                     `.:/:                   yhhho                  `//-.                           
+'               `-:+oyhhhhdyyo:`             `+hhy/              -sy+oyhdddys+:.`                    
+'           -+syhhhdddddddhh/--\:::::::::::::+dhhdh:::::::::::/sd/--\sssssyhhdddhs+`                
+'          .hdddmmddhyysssss|  |yddddddhhhhhhddddddhhhhhhhhhhhddh|  |hhhhyyysssssyhhs
+'        .dmmdhyo+/// +++oyh|SH|hdhhhhhhhhhhhddhhddhdddddddddddmh|OP|dhhhhdddhhhhyyys +`              
+'        `hmdso++/+//+++ooyd|09|dNmmmmmmmmmmm      mmmmmmmmmmmmNh|09|dhhhhd/----\ddddss/              
+'        smmyo+++ossssssyyys\--/dmmmmmmmmmmmm  PD  mmmmmmmmmmmmNNh\--/yyhhdh| △ |dddyss:              
+'       +mmdsoshmmmmmmmdyyyyyhhhdmmmmmmmmmmmm  13  mmmmmmmmmmmmNhhhhhhhhhdd| TR |dddddsss.            
+'      -mmmyshmNm  +y mNdyhhhhhdNmmmmmmmmmmmm      mmmmmmmmmmNNNhhhhd/----\| 03 |/----\so`           
+'      hmmdshmNm   ↑   mNhyhhhhdmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmNhhddy| □ |\----/| 02 |yys/           
+'     mmmysdmN -x←10→+x NyhhhhdmmmmmmmmmmmNmmmNmmmNmmmNNmmNNNdhddsh| SQ |dddddm| CI |dyys.
+'    `hmmdyshhNNm  ↓   Nhhhhhhhmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmhhhdd| 00 |dddddd| ○ |dhyy+          
+'    mmmdyyyyydNNm -y Nmhyhhhhhhhhhhhyyysydddhhhhddddyyyyyhhhhhhhhhdd\----//----\\----/hhhs.
+'    ommmdhyyyyyyhddddhyyhhhhhhmmddmdhhhhhmddmdhhhmddmdddhhdmmmdhmmdhddddmh| 01 |ddddddhhhy
+'   .ymmmddhyysyyyyyyyhhhhhhhhhNdddmmhhhhhddddhhhhddmddhdmNNm     Nmhdddddd| CR |ddddddhddh+`        
+'   /hmmmdddhyyyyyyyyhhhhhhmdddmddddmddddhhhdhhhdddddddhNNmm   ↑  NNmhdddd| ☓ |dddddddddhs-        
+'  `ohmmmmdddhyyyyyyyhhhhhdmddddddhhhhhhNhhhhh/----\mdddddNN ←11→  Nddddd\----/dddddddddhs/        
+'  -shmmmmdddddhhyyyhhhhhhdmmmmmdhhhddddmhhhhd| PS |ddddmNmmm  ↓  NNyhdddddddddddddddddddhyo`       
+'  /ydmmmmmmdddddddhhhhhhhhddddNdhhdmddddhhhhh| 12 |dddhyhNmmN    NNhyddddddddddddddddddddys-       
+' `oydmmmmmmmmdddddddddddddddddNdddmmhhhhyyssh\----/dydddyyhddmmddhyhddddddddddddddddddddddhs/       
+' .shdmmmmmmmmmddddddddddddhhhhhhhhyyssssssssoooosoosssssssoo+ooosyhhddddddddddddddddddddddhy +`      
+' -shdmmmmmmmmmmmmddddhhyyssoo++//:--....```````````````...--://+oossyyyhdddmmmmddddddmdddhyo.      
+' /yhdmmmmmmmmmmddddhyyssoo+//:-.``                           ``.-:/++ossyyhhddddddddmdddddhys-      
+' +yhdmmmmmmmmdddhhhyysso+/:-.`                                   `.-/+oosyyyhhhdddddddddddhys:      
+' +yhddddmmdddhhhhyyysso+/-`       v min mid max                    `.-/+ossyyhhhhhdddddddhhys/      
+'`oyhhddddddhhhhhyyyso+/-`         x   0  50 100                       .:+osyyyyhhhhhhdddhhhys/      
+'`+yyhhhhhhhhhhyyyyso+:.           y   0  50 100                         -/ossyyyyhhhhhhhhhhys/      
+'`+yyhhhhhhhhyyyyyso+-`            z   0  50 100                          `:+ssyyyyyhhhhhhhhys/      
+' +syyyyhyyyyyyyyso/.                                                       -+osyyyyyyyhhhyyys      
+' osyyyyyyyyysso+: `                                                         ./ossyyyyyyyyyyso-      
+' `+osyyyysssso+:.                                                             ./oosssyyyysso/`      
+'  .: +oooooo+/-.                                                                 .:/+oooooo+:`       
+'    `..--..`                                                                       `......`         
+
+Imports SlimDX.DirectInput
+Imports System.IO.Ports
+
+Public Class EV3
+    Private m_JoysTick As Joystick
+    Private m_JoysticSt As JoystickState
+    Private N As Integer = 0
+    Dim tex As String = ""
+    Private ReadOnly SQUARE As Integer = 0
+    Private ReadOnly CROSS As Integer = 1
+    Private ReadOnly CIRCLE As Integer = 2
+    Private ReadOnly TRIANGLE As Integer = 3
+    Private ReadOnly L1 As Integer = 4
+    Private ReadOnly R1 As Integer = 5
+    Private ReadOnly L2 As Integer = 6
+    Private ReadOnly R2 As Integer = 7
+    Private ReadOnly SHARE As Integer = 8
+    Private ReadOnly OPTIONS As Integer = 9
+    Private ReadOnly L_STICK As Integer = 10
+    Private ReadOnly R_STICK As Integer = 11
+    Private ReadOnly PS As Integer = 12
+    Private ReadOnly PAD As Integer = 13
+
+    Private SLX As Integer = 0
+    Private SLY As Integer = 0
+    Private SRX As Integer = 0
+    Private SRY As Integer = 0
+    Private POV As Integer = 0
+
+    Private SL2 As Integer = 0
+    Private SR2 As Integer = 0
+
+    Private Sub GamePad_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Debug_Send("プログラムを起動しました。")
+        Dim manager As New DirectInput
+        Dim timer As Timer = New Timer()
+        'ゲームパッド検索
+        Debug_Send("コントローラーの検索中．．．")
+        For Each wDeviceInstance As DeviceInstance In
+            manager.GetDevices(DeviceClass.GameController, DeviceEnumerationFlags.AttachedOnly)
+            m_JoysTick = New Joystick(manager, wDeviceInstance.InstanceGuid)
+            Exit For
+        Next wDeviceInstance
+        If m_JoysTick Is Nothing Then
+            Dim unused0 = MessageBox.Show("コントローラが見つかりません！！", Text, MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Close()
+        Else
+            Debug_Send("コントローラーが見つかりました。")
+            Debug_Send("ボタンの割り当て中．．．")
+            '入力値範囲設定
+            For Each wObjects As DeviceObjectInstance In m_JoysTick.GetObjects
+                If (wObjects.ObjectType And ObjectDeviceType.Axis) <> 0 Then
+                    m_JoysTick.Properties.SetRange(0, 100)
+                End If
+            Next wObjects
+            '協調レベル(バックグラウンドでも受け付ける)
+            Dim unused1 = m_JoysTick.SetCooperativeLevel(Me, CooperativeLevel.Nonexclusive Or CooperativeLevel.Background)
+            Dim unused2 = m_JoysTick.Acquire() '情報取得
+            'タイマー開始
+            AddHandler timer.Tick, New EventHandler(AddressOf Timer_Tick)
+            timer.Interval = 100
+            timer.Enabled = True
+            Debug_Send("シリアルポートを検索中．．．")
+            For Each sp As String In My.Computer.Ports.SerialPortNames
+                Dim unused = ComNum.Items.Add(sp)
+            Next
+            ComNum.SelectedIndex = 0
+            Debug_Send("シリアルポートを選択して「接続！」を押してください。")
+        End If
+    End Sub
+
+    Private Sub Form_FormClosed(sender As Object, e As FormClosedEventArgs) Handles Me.FormClosed
+        SerialPort.Close() 'シリアルポートクローズ
+    End Sub
+    Private Sub Controller_Click(sender As Object, e As EventArgs) Handles Controller.Click
+        Select Case N Mod 3
+            Case 0
+                Controller.Image = My.Resources.Blue
+                Exit Select
+            Case 1
+                Controller.Image = My.Resources.Black
+                Exit Select
+            Case 2
+                Controller.Image = My.Resources.Red
+                Exit Select
+            Case Else
+                Exit Select
+        End Select
+        N += 1
+    End Sub
+
+    Private Sub Connect_Click(sender As Object, e As EventArgs) Handles Connect.Click
+        Debug_Send("シリアルポート" + ComNum.SelectedItem + "を開いています．．．")
+        With SerialPort
+            'シリアルポート設定  
+            .PortName = ComNum.SelectedItem 'ポート名  
+            .BaudRate = 115200              '通信速度指定  
+            .Parity = Parity.None           'パリティ指定  
+            .DataBits = 8                   'ビット数指定  
+            .Handshake = Handshake.None
+            .StopBits = StopBits.One        'ストップビット指定
+            .Encoding = System.Text.Encoding.UTF8
+            .Open()                         'シリアルポートオープン  
+        End With
+        Debug_Send("シリアルポートを開きました。" + vbCrLf + "ロボットに接続中．．．．．．" + vbCrLf + "------------------------------------------")
+        ComNum.Enabled = False
+        Connect.Enabled = False
+        Debug_Send(tex)
+    End Sub
+
+    Delegate Sub AddText(ByVal Text As String)
+    Private Sub SerialPort1_DataReceived(sender As Object, e As SerialDataReceivedEventArgs) Handles SerialPort.DataReceived
+        Dim unused1 = ConsoleBox.Invoke(New AddText(AddressOf ConsoleBox.AppendText), SerialPort.ReadLine + " のロボットに接続しました。")
+    End Sub
+
+    Private Sub Debug_Send(sendStr As String)
+        ConsoleBox.AppendText(sendStr + vbCrLf)
+        Console.WriteLine(sendStr)
+    End Sub
+
+    Private Sub Serial_Send(sendStr As String)
+        If SerialPort.IsOpen Then
+            SerialPort.Write(sendStr) ' シリアル送信
+        End If
+    End Sub
+
+    Private Sub Timer_Tick(sender As Object, e As EventArgs)
+        m_JoysticSt = m_JoysTick.GetCurrentState '状態取得
+        'ボタン状態送信
+        Dim bs As Char() = New Char() {"0"c, "0"c, "0"c, "0"c, "0"c, "0"c, "0"c, "0"c, "0"c, "0"c, "0"c, "0"c, "0"c, "0"c, "0"c, "0"c, "0"c, "0"c, "0"c, "0"c, "0"c}
+        For i As Integer = SQUARE To PAD
+            bs(i) = If(m_JoysticSt.GetButtons(i), "1"c, "0"c)
+        Next i
+
+        SL2 = m_JoysticSt.RotationX
+        SR2 = m_JoysticSt.RotationY
+        SLX = m_JoysticSt.X
+        SLY = 100 - m_JoysticSt.Y
+        SRX = m_JoysticSt.Z
+        SRY = 100 - m_JoysticSt.RotationZ
+        POV = If(m_JoysticSt.GetPointOfViewControllers(0) = -1, 0, (m_JoysticSt.GetPointOfViewControllers(0) / 300) + 1) '十字キー(-1:離す, 0上, 9000右, 18000下, 27000左)
+        Console.WriteLine("stick  : LX" + SLX.ToString + " LY" + SLY.ToString + " RX" + SRX.ToString + " RY" + SRY.ToString)
+        Console.WriteLine("trigger: LT" + m_JoysticSt.RotationX.ToString + " RT" + m_JoysticSt.RotationY.ToString)
+        Console.WriteLine("cross  : " + If(m_JoysticSt.GetPointOfViewControllers(0) = -1, "-1", (m_JoysticSt.GetPointOfViewControllers(0) / 100).ToString) + "°")
+
+        bs(14) = Chr(SL2) '左肩の感圧
+        bs(15) = Chr(SR2) '右肩の感圧
+        bs(16) = Chr(SLX) '左スティック
+        bs(17) = Chr(SLY) '左スティック
+        bs(18) = Chr(SRX) '右スティック
+        bs(19) = Chr(SRY) '右スティック
+        bs(20) = Chr(POV) '十字キー
+        Console.WriteLine("bs send: " + bs)
+        Serial_Send("b"c)
+        Serial_Send(bs)
+        State.Text = "スティック     左X" + SLX.ToString + " 左Y" + SLY.ToString + " 右X" + SRX.ToString + " 右Y" + SRY.ToString + vbCrLf +
+                     "肩ボタン     左肩" + m_JoysticSt.RotationX.ToString + " 右肩" + m_JoysticSt.RotationY.ToString + vbCrLf +
+                     "十字キー    " + If(m_JoysticSt.GetPointOfViewControllers(0) = -1, "押されていない", ((m_JoysticSt.GetPointOfViewControllers(0) / 100).ToString) + "°") + vbCrLf +
+                     "送信データ  b" + bs
+    End Sub
+End Class
